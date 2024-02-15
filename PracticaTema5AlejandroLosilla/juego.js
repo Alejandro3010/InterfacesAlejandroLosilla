2790ece04827b9abb17890407fd0166d7969f457
Alejandro3010 <127496878+Alejandro3010@users.noreply.github.com>
2024-02-15 10:09:03 +0100

    Add files via upload

--- PracticaTema5AlejandroLosilla/juego.js
+++ PracticaTema5AlejandroLosilla/juego.js
@@ -2,9 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
   document.querySelector('.loader').style.display = 'block';
   document.querySelector('.contenido').style.display = 'none';
+  document.querySelector('.bg').style.display = 'none';
 
   setTimeout(function() {
     document.querySelector('.loader').style.display = 'none';
     document.querySelector('.contenido').style.display = 'block';
+    document.querySelector('.bg').style.display = 'block';
   }, 4000);
 });
