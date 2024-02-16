bb2642ec60561973d985c19e597559979cb8755f
Alejandro3010 <127496878+Alejandro3010@users.noreply.github.com>
2024-02-16 19:35:11 +0100

    Add files via upload

--- PracticaTema5AlejandroLosilla/juego.js
+++ PracticaTema5AlejandroLosilla/juego.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", function() {
   document.querySelector('.loader').style.display = 'block';
   document.querySelector('.contenido').style.display = 'none';
   document.querySelector('.bg').style.display = 'none';
+  document.querySelector('.suelo').style.display = 'none';
+  document.querySelector('.cielo').style.display = 'none';
+  document.querySelector('.cielo1').style.display = 'none';
+  document.querySelector('.cielo2').style.display = 'none';
 
   setTimeout(function() {
     document.querySelector('.loader').style.display = 'none';
@@ -10,3 +14,12 @@ document.addEventListener("DOMContentLoaded", function() {
     document.querySelector('.bg').style.display = 'block';
   }, 4000);
 });
+
+function jugar() {
+  document.querySelector('.contenido').style.display = 'none';
+  document.querySelector('.suelo').style.display = 'block';
+  document.querySelector('.bg').style.display = 'none';
+  document.querySelector('.cielo').style.display = 'block';
+  document.querySelector('.cielo1').style.display = 'block';
+  document.querySelector('.cielo2').style.display = 'block';
+}
